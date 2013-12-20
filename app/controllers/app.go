d309d46bb98b6fa419a020472efa0a37e6f5829c
Jackson Owens <jackson_owens@brown.edu>
2013-12-19 19:49:01 -0500

    confusion

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,34 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+    "database/sql"
+    "log"
+    "github.com/jbowens/taboo/app/model"
+    "github.com/robfig/revel"
+    _ "github.com/lib/pq"
+)
 
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+
+    db, err := sql.Open("postgres", "user=taboo dbname=taboo password=tababooboo sslmode=disable")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    rows, err := db.Query("SELECT * FROM words ORDER BY random() LIMIT 1")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    rows.Next()
+
+    var wordRow Word
+    rows.Scan(&wordRow)
+    word := wordRow.word
+
+	return c.Render(word)
 }
