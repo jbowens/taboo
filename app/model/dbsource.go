13c3a20cde0bda0cf28b02f2a36371defb545b4f
Jackson Owens <jackson_owens@brown.edu>
2013-12-19 21:47:31 -0500

    what is this language

---
+++ app/model/dbsource.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+    "database/sql"
+)
+
+type DatabaseWordSource struct {
+    db *DB
+}
+
+func RandomWord(source *DatabaseWordSource) (*Word, error) {
+    rows, err := source.db.Query("SELECT * FROM words ORDER BY random() LIMIT 1")
+
+    if err != nil {
+        return nil, err
+    }
+
+    var word *Word = new(Word)
+    rows.Next()
+    err = rows.scan(&Word.Id, &Word.Word)
+
+    return word, err
+}
+
+func AllWords(source *DatabaseWordSource) ([]Word, error) {
+    rows, err := source.db.Query("SELECT * FROM words")
+
+    if err != nil {
+        return nil, err
+    }
+
+    var words []Word
+
+    for rows.Next() {
+        var word *Word = new(Word)
+        err = rows.Scan(&word.Id, &word.Word)
+        if err != nil {
+            return nil, err
+        }
+        append(words, word)
+    }
+
+    return words, nil
+}
---
+++ app/model/wordsource.go
@@ -0,0 +1,12 @@
+package model
+
+// An interface for accessing words.
+type WordSource interface {
+
+    // Retrieves a random word
+    RandomWord() (*Word, error)
+
+    // Retrieves all of the words from this word source
+    AllWords() ([]Word, error)
+
+}
