237e95e0fbf2fd66b9c75575c9786a3c66542a05
Jackson Owens <jackson_owens@brown.edu>
2013-12-20 20:22:06 -0500

    auto-detected 100 stop words

--- wordgen/osx-dict/explore.py
+++ wordgen/osx-dict/explore.py
@@ -2,14 +2,16 @@
 from stemming.porter2 import stem
 import DictionaryServices, re, operator
 
-ignore_list = set(['the', 'noun', 'adjective', 'the', 'for', 'not', 'when', 'shall', 'will', 'and', 'with', 'brit', 'informal', 'but', 'that', 'from', 'verb', 'their'])
+#ignore_list = set(['the', 'noun', 'adjective', 'the', 'for', 'not', 'when', 'shall', 'will', 'and', 'with', 'brit', 'informal', 'but', 'that', 'from', 'verb', 'their', 'derivatives', 'adverb', 'late', 'latin', 'origin'])
 
 start = "love"
 max_depth = 10
 min_word_len = 4
+most_freq_cutoff = 100
 
 visited = set([])
 freqs = dict()
+unigram_freqs = dict()
 
 def visit(word, depth):
     if depth > max_depth:
@@ -33,16 +35,27 @@ def visit(word, depth):
     
     for w in words:
         w_stem = stem(w)
-        if w not in ignore_list and len(w) >= min_word_len and w != word and w_stem != word_stem:
+        if w != word and w_stem != word_stem:
             if not w in freqs:
                 freqs[w] = dict()
 
             freqs[word][w] = 1 if w not in freqs[word] else freqs[word][w] + 1
             freqs[w][word] = 1 if word not in freqs[w] else freqs[w][word] + 1
+            unigram_freqs[w] = 1 if w not in unigram_freqs else unigram_freqs[w] + 1
             
             visit(w, depth + 1)
 
 visit(start, 1)
+
+# Find the words that appeared most frequently. They're probably shit.
+shit_words = map(lambda x: x[0], sorted(unigram_freqs.iteritems(), key=operator.itemgetter(1), reverse=True)[0:most_freq_cutoff])
+
+# Remove shit words
+for shit_word in shit_words:
+    del freqs[shit_word]
+    for w in freqs:
+        if shit_word in freqs[w]:
+            del freqs[w][shit_word]
 
 sorted_words = sorted(freqs.keys())
 for word in sorted_words:
