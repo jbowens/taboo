36f3d39618258fd6b5cefb0332160792ed52308b
Jackson Owens <jackson_owens@brown.edu>
2013-12-23 17:56:41 -0500

    word importer; takes json and inserts it into the db

---
+++ wordgen/data-importer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys, json, psycopg2
+
+CONN_STR = 'dbname=prod user=prod'
+
+data_str = '\n'.join(sys.stdin.readlines())
+data = json.loads(data_str)
+
+conn = psycopg2.connect(CONN_STR)
+
+cur = conn.cursor()
+count = 0
+for word in data:
+    cur.execute("INSERT INTO words (word, skipped, correct) VALUES(%s, %s, %s)",
+            (word, 0, 0))
+    wordid = cur.lastrowid
+    for prohibited in data[word]:
+        cur.execute("INSERT INTO prohibited_words (wordid, word) VALUES(%s, %s)",
+                (wordid, prohibited))
+    count = count + 1
+
+conn.commit()
+cur.close()
+conn.close()
+
+print 'Inserted ' + str(count) + ' words'
