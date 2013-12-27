831e334f52c4f6bd0ba37db65aa6ca762efc158b
Jackson Owens <jackson_owens@brown.edu>
2013-12-27 03:30:09 -0500

    added data exporter script

---
+++ wordgen/data-exporter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import sys, json, psycopg2, argparse
+
+parser = argparse.ArgumentParser(description='Exports word data from the database to a json file.')
+parser.add_argument('--verified-only', dest='verified', action='store_true', help='if provided, only verified words will be exported')
+args = parser.parse_args()
+
+CONN_STR = 'dbname=prod user=prod'
+
+conn = psycopg2.connect(CONN_STR)
+
+cur = conn.cursor()
+
+if args.verified:
+    cur.execute("SELECT id, word FROM words WHERE verified")
+else:
+    cur.execute("SELECT id, word FROM words")
+
+words = [];
+
+rows = cur.fetchall()
+for row in rows:
+    w = dict()
+    w['id'] = row[0]
+    w['word'] = row[1]
+
+    cur.execute("SELECT word FROM prohibited_words WHERE wordid = " + str(row[0]))
+    prohibited_words = map(lambda x: x[0], cur.fetchall())
+
+    w['prohibited'] = prohibited_words
+    words.append(w);
+
+conn.commit()
+cur.close()
+conn.close()
+
+print json.dumps(words)
