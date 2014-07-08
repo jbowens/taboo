ccbee610aa4aa6b78e3c7b1ee97c7b9f39c82fbc
Jackson Owens <jackson_owens@brown.edu>
2014-07-08 15:53:05 -0700

    removed dumb website shit

--- app/model/session.go
+++
@@ -1,8 +0,0 @@
-package model
-
-// Stores information about what cards have been played in a
-// user's session. It's used to prevent repeat cards within
-// the same session.
-type Session struct {
-  
-}
