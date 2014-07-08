14b51c05ca62d4e786d881e1b1c9fa3ed722b143
Jackson Owens <jackson_owens@brown.edu>
2014-07-08 15:49:02 -0700

    added filtering of existing words

--- wordgen/wordassoc/parser.py
+++ wordgen/wordassoc/parser.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
-import sys, json, re
+import sys, json, re, psycopg2
 
+CONN_STR = 'dbname=prod user=prod'
 digits = re.compile('\d')
+
+conn = psycopg2.connect(CONN_STR)
+conn.autocommit = True
+cur = conn.cursor()
 
 def valid_word(word):
     if bool(digits.search(word)):
@@ -10,10 +15,15 @@ def valid_word(word):
     if len(word) < 4:
         # Only want longer words
         return False
-    return True
+
+    # Make sure the word doesn't already exist.
+    cur.execute("SELECT COUNT(word) FROM words WHERE word = %s;", (word,))
+    exists = cur.fetchone()[0]
+    return not exists
 
 with open('rawdata.txt') as f:
     lines = f.read().lower().splitlines()
     words = dict(filter(lambda y: valid_word(y[0]), zip(lines[::2], map(lambda x: x.split('|')[:20:2], lines[1::2]))))
+
     for w in words:
         print w + ': ' + ', '.join(words[w])
