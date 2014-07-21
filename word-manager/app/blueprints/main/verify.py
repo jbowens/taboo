c650479ca7fbbcc97658c7625139e1f470ef0380
Jackson Owens <jackson_owens@alumni.brown.edu>
2014-07-20 21:49:23 -0700

    random word every verify

--- word-manager/app/blueprints/main/verify.py
+++ word-manager/app/blueprints/main/verify.py
@@ -2,10 +2,11 @@ from app.blueprints.main import main
 from app.models.word import Word
 from app import db
 from flask import render_template, request, current_app
+from sqlalchemy import func
 
 @main.route('/verify-words', methods=['GET'])
 def verify_words():
-  word = Word.query.filter(Word.status=='unverified').first()
+  word = Word.query.filter(Word.status=='unverified').order_by(func.random()).first()
   if word:
     return render_template('verify_word.html', \
             word=word, \
