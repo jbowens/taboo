703b0e74d7b0ecba51056f19e5156958eda2bde9
Jackson Owens <jackson_owens@alumni.brown.edu>
2014-07-24 13:28:53 -0700

    split up query onto multiple lines

--- word-manager/duplicates-query.sql
+++ word-manager/duplicates-query.sql
@@ -1,1 +1,4 @@
-select a.wid, a.word, a.status, b.wid, b.status from words AS a INNER JOIN words AS b ON lower(a.word) = b.word AND a.wid != b.wid;
+select a.wid, a.word, a.status, b.wid, b.status
+from words AS a
+INNER JOIN words AS b
+ON lower(a.word) = b.word AND a.wid != b.wid;
